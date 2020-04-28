7420050cd933b770f4f14202689100568091615b
Erick Feng <fengzibao@gmail.com>
2020-04-28 11:16:40 -0700

    Unit tests by Junit and SpringTest added.

---
+++ spring03wired-auto/src/main/java/SoundSystem/AppConfig.java
@@ -0,0 +1,13 @@
+package SoundSystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Erick 2020.04.27:14:59
+ * spring03wired-auto configuration class.
+ */
+@Configuration
+@ComponentScan
+public class AppConfig {
+}
