129a9da2a5acb556ad1c8d29ea95f6ac9ae57a5c
Erick Feng <fengzibao@gmail.com>
2020-04-28 20:53:09 -0700

    @Autowired with @Resource(name = first low cased letter Class/Type name).

--- spring04wiring-interface/src/test/java/com/hotai/demo/service/UserServiceTest.java
+++ spring04wiring-interface/src/test/java/com/hotai/demo/service/UserServiceTest.java
@@ -7,11 +7,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class UserServiceTest {
-    @Autowired
-    @Qualifier("userServiceNormal")
+//    @Autowired
+//    @Qualifier("userServiceNormal")
+    @Resource(name="userServiceFestival")
     private UserService userService;
 
     @Test
