4a854c36b714e61da89d949c0175612e16e43d5e
Erick Feng <fengzibao@gmail.com>
2020-04-30 11:51:40 -0700

    Defined class path for component scan in Spring app configuration file of applicationContext.xml under resources folder.

--- spring04wiring-interface/src/test/java/com/hotai/demo/web/UserControllerTest.java
+++ spring04wiring-interface/src/test/java/com/hotai/demo/web/UserControllerTest.java
@@ -11,7 +11,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by Erick 2020.04.30:10:01
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes= AppConfig.class)
+//@ContextConfiguration(classes= AppConfig.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class UserControllerTest {
 
     @Autowired
